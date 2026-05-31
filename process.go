3628eeb39e5747bbbda710f3095b828ca8f9e0a9
lipkerton <petr.petuxof@yandex.ru>
2026-05-31 21:36:22 +0300

    process

---
+++ process.go
@@ -0,0 +1,28 @@
+package main
+import (
+	"os"
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	arguments := os.Args
+	var total, nInts, nFloats int
+	invalid := make([]string, 0)
+	for _, k := range arguments[1:] {
+		_, err := strconv.Atoi(k)
+		if err == nil {
+			nInts++
+			total++
+			continue
+		}
+		_, err = strconv.ParseFloat(k, 64)
+		if err == nil {
+			nFloats++
+			total++
+			continue
+		}
+		invalid = append(invalid, k)
+	}
+	fmt.Println(invalid)
+}
