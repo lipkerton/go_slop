84628433f417729dd0c2bb42ecbecaeb8700cd2d
lipkerton <petr.petuxof@yandex.ru>
2026-06-06 15:55:24 +0300

    error

--- ch02/error.go
+++ ch02/error.go
@@ -8,7 +8,43 @@ import (
 
 func check(a, b int) error {
 	if a == b && b == 0 {
-		return error.New("this is custom error message")
+		return error.New("this is a custom error message")
 	}
 	return nil
+}
+
+func formatedError(a, b int) error {
+	if a == 0 && b == 0 {
+		return fmt.Errorf("a %d and b %d. UserID: %d", a, b, os.Getuid())
+	}
+	return nil
+}
+
+func main() {
+	err := check(0, 10)
+	if err == nil {
+		fmt.Println("check() ended normally!")
+	} else {
+		fmt.Println(err)
+	}
+	
+	err = check(0, 0)
+	if err.Error() == "this is a custom error message" {
+		fmt.Println("Custom error detected!")
+	}
+
+	err = formatedError(0, 0)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	i, err := strconv.Atoi("-123")
+	if err == nil {
+		fmt.Println("Int value is", i)
+	}
+
+	i, err = strconv.Atoi("Y123")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
