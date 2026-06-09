2b24e5099bb2d22922b35ecb822c405ea2622cc1
lipkerton <petr.petuxof@yandex.ru>
2026-06-09 10:49:18 +0300

    useStrings

---
+++ ch02/useStrings.go
@@ -0,0 +1,19 @@
+package main
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	fmt.Printf("EqualFold: %v\n", strings.EqualFold("Cool", "COol"))
+	
+	fmt.Printf("Index: %v\n", strings.Index("Cool", "o"))
+	
+	fmt.Printf("Prefix: %v\n", strings.HasPrefix("Cool", "Co"))
+
+	t := string.Fields("String with spaces!")
+	fmt.Println("Fields: %v\n", t)
+
+	fmt.Printf("%s\n", strings.Split("abcd efg", ""))	
+}
